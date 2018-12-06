085c35928e68bc8c97ac14ded2bd7f6d4c01af59
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-12-06 23:15:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+# def prime? (number)
+#   divisor = 2
+#   while (number > divisor)
+#     if number % divisor == 0
+#       return false
+#     else if number < 0
+#       return false
+#     else
+#       divisor += 1
+#     end
+#   end
+#   return true
+# end
+
+def prime? (number)
+  divisor = 2
+  while (number > divisor)
+    if (number % divisor == 0) || (number < 0)
+      return false
+    else
+      divisor += 1
+    end
+  end
+  return true
+end
